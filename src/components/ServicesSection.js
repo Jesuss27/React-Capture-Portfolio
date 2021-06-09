3ee9c158aed0d7cdf2be20500c66b8911b869a67
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-06-09 10:39:22 -0700

    ServicesSection

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React from "react"; 
+import clock from "../images/clock.svg"
+import diaphragm from "../images/diaphragm.svg"
+import money from "../images/money.svg"
+import teamwork from "../images/teamwork.svg"
+import workStation from "../images/workStation.jpg"
 
 const ServicesSection = () => {
     return(
@@ -7,17 +12,40 @@ const ServicesSection = () => {
                 <h2>Top <span>Quality</span> Service</h2>
                 <div className="cards">
                     <div className="card">
-                        <div className="icon"></div>
+                        <div className="icon">
+                            <img src={clock} alt="" />
+                            <h3>Efficient</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
                     </div>
+
                     <div className="card">
-                        <div className="icon"></div>
+                        <div className="icon">
+                            <img src={diaphragm} alt="" />
+                            <h3>Precise</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
                     </div>
+
                     <div className="card">
-                        <div className="icon"></div>
+                        <div className="icon">
+                            <img src={teamwork} alt="" />
+                            <h3>Cooperative</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
                     </div>
+
                     <div className="card">
-                        <div className="icon"></div>
+                        <div className="icon">
+                            <img src={money} alt="" />
+                            <h3>Affordable</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
                     </div>
+                   
+                </div>
+                <div className="image">
+                    <img src={workStation} alt="" />
                 </div>
             </div>
         </div>
