d56923ef64c0e413467a3bfe691a6b60b474920e
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-06-09 11:13:51 -0700

    FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,29 @@
+import React from "react" ;
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h1>Qualifications</h1>
+            <div className="proficiencies">
+                <div className="proficiency">
+                    <h2>Javascript</h2>
+                </div>
+                <div className="proficiency">
+                    <h2>HTML</h2>
+                </div>
+                <div className="proficiency">
+                    <h2>SASS/SCSS</h2>
+                </div>
+                <div className="proficiency">
+                    <h2>REACT</h2>
+                </div>
+                <div className="proficiency">
+                    <h2>PHP</h2>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default FaqSection
