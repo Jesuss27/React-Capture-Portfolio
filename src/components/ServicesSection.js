ac30e3c9b7801360764e45d716e21c26370b9d3e
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-06-09 10:27:15 -0700

    Create ServicesSection and add SVGs

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ServicesSection = () => {
+    return(
+        <div className="services">
+            <div className="description">
+                <h2>Top <span>Quality</span> Service</h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon"></div>
+                    </div>
+                    <div className="card">
+                        <div className="icon"></div>
+                    </div>
+                    <div className="card">
+                        <div className="icon"></div>
+                    </div>
+                    <div className="card">
+                        <div className="icon"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default ServicesSection
