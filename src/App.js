b7041172abf9ca4427502b5bf1ca7f33f4808137
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-06-14 09:29:49 -0700

    Route Product Detail

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from "./pages/Contact" ;
 import GlobalStyle from "./components/GlobalStyle"
 import Nav from "./components/Nav";
 import {Switch,Route} from "react-router-dom";
+import ProductDetail from "./pages/ProductDetail"
 
 
 function App() {
@@ -17,7 +18,11 @@ function App() {
           <AboutUs/>
         </Route>
 
-        <Route path="/work">
+        <Route path="/work/:id" >
+          <ProductDetail />
+        </Route>
+
+        <Route path="/work" exact>
           <Work/>
         </Route>
 
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import {useHistory} from "react-router-dom"
+
+const ProductDetail = () => {
+    return(
+        <div>
+            <h1>
+                Product Detail
+            </h1>
+        </div>
+
+    )
+
+}
+
+export default ProductDetail
