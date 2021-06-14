0c27768486b23c8fff3b4a99e599eda37a437df0
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-06-14 09:07:18 -0700

    add ProductState

---
+++ src/components/ProductState.js
@@ -0,0 +1,75 @@
+import product1 from "../images/product1.webp"
+import product2 from "../images/product2.webp"
+import product3 from "../images/product3.webp"
+
+
+export const ProductState = () => {
+    return [
+      {
+        title: "Goodfika",
+        mainImg: product1,
+        url: "/work/goodfika",
+        awards: [
+          {
+            title: "Truly A masterpiece",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+          {
+            title: "Fresh look on a brutal sport.",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+          {
+            title: "It’s okay lmao.",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+        ],
+      },
+      {
+        title: "Sad Times",
+        mainImg: product2,
+        url: "/work/sad-times",
+        awards: [
+          {
+            title: "Truly A masterpiece",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+          {
+            title: "Fresh look on a brutal sport.",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+          {
+            title: "It’s okay lmao.",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+        ],
+      },
+      {
+        title: "Hamlet",
+        mainImg: product3,
+        url: "/work/hamlet",
+        awards: [
+          {
+            title: "Truly A masterpiece",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+          {
+            title: "Fresh look on a brutal sport.",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+          {
+            title: "It’s okay lmao.",
+            description:
+              "“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”",
+          },
+        ],
+      },
+    ];
+  };
