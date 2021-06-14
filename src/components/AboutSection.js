ca2eb34a7b5952e22bbff0736aec31fa107122fc
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-06-14 11:54:53 -0700

    Basic Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,28 +1,33 @@
 import React from "react" ;
 import wireframe from "../images/wireframe.jpg";
-//styled
 import {About, Description, Hide, Image} from "../styles"
+//Framer Motion
+
+import { motion } from "framer-motion";
+
+
 const AboutSection = () => {
+
     return(
             <About>
                 <Description>
-                    <div className="title">
+                    <motion.div>
                          <Hide>
-                             <h2>
+                             <motion.h2>
                                  I <span>develop</span> and <span className="design">design</span>
-                             </h2>
+                             </motion.h2>
                          </Hide>
                          <Hide>
-                             <h2>
+                             <motion.h2>
                                  whatever <span>YOU</span>
-                             </h2>
+                             </motion.h2>
                          </Hide>
                          <Hide>
-                             <h2>
+                             <motion.h2>
                                  have in mind
-                             </h2>
+                             </motion.h2>
                          </Hide>
-                    </div>
+                    </motion.div>
                     <p>If you are looking to hire a professional with hard skills and a gentle touch to help you build out your next big web project, get in touch with me. </p>
                     <button>CONTACT</button>
                 </Description>
